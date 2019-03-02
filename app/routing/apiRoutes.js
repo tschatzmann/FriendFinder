ccaa4687566da3e9d8e11cb5a9d4bb4888093a42
tschatzmann <tarrta2002@gmail.com>
2019-03-02 15:26:13 -0500

    posting logic

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -23,6 +23,14 @@ module.exports = function(app) {
     res.json(friendsData);
   });
 
+  app.post("/api/friends", function(req, res) {
+    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+    // It will do this by sending out the value "true" have a table
+    // req.body is available since we're using the body parsing middleware
+    friendsData.push(req.body);
+    console.log(friendsData)
+      res.json(true);
+  });
 
   // If no matching route is found default to home
 
